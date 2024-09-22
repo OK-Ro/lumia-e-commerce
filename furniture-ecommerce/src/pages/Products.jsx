3360425b50077f9c2ea19968d89e3de68162f8f3
OK-Ro <o.robert1994@hotmail.com>
2024-09-22 13:54:54 +0200

    lumia-site

---
+++ furniture-ecommerce/src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div className="products">
+      <h1>Our Furniture Collection</h1>
+      {/* Product list will go here */}
+    </div>
+  );
+};
+
+export default Products;
